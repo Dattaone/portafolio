553bceece86166c0fa71ccfc0af8d5e2d4090e84
Dattaone <chepogor@gmail.com>
2024-06-14 22:03:43 -0500

    add Experience

---
+++ tailwind.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+import {skills} from "./cv.json";
+const width = (-98.5 * skills.length)/9
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				tsBlue: '#007acc',
+				gunmetal: '#2D3142'
+			},
+			keyframes: {
+				scroll: {
+				  "0%": { transform: "translateX(0)" },
+				  "100%": { transform: `translateX(${width}%)` },
+				},
+			},
+			animation: {
+				scroll: "scroll 20s linear infinite",
+			},
+		},
+	},
+	plugins: [
+		require('tailwind-scrollbar'),
+	],
+}
