65467fc578429be15bb488a8ef519ac62c79a3c8
Dattaone <chepogor@gmail.com>
2024-07-01 00:12:16 -0500

    beta-1

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import {skills} from "./cv.json";
-const width = (-98.5 * skills.length)/9
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -8,15 +6,6 @@ export default {
 		extend: {
 			colors:{
 				gunmetal: '#2D3142'
-			},
-			keyframes: {
-				scroll: {
-				  "0%": { transform: "translateX(0)" },
-				  "100%": { transform: `translateX(${width}%)` },
-				},
-			},
-			animation: {
-				scroll: "scroll 20s linear infinite",
 			},
 		},
 	},
